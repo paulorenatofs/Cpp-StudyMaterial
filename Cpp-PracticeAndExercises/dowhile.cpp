f82e3927164542935b438d00aea59cf4b94acf51
prpaschoal-barbieri <prpaschoal-barbieri@myseneca.ca>
2025-05-10 13:47:34 -0400

    including extra loop exercises

--- Cpp-PracticeAndExercises/dowhile.cpp
+++ Cpp-PracticeAndExercises/dowhile.cpp
@@ -36,9 +36,28 @@ void find_first_vowel(const std::vector<char>& vec)
     
 }
 
+int calculate_pairs(std::vector<int> vec) {
+    int result{};
+
+    std::cout << "\n---------------------" << std::endl;
+    for (size_t i{ 0 }; i < vec.size(); i++)
+    {
+        for (size_t j{ i + 1 }; j < vec.size(); j++)
+        {
+            result += vec.at(i) * vec.at(j);
+            std::cout << "Running total: " << vec.at(i) << " * " << vec.at(j) << " = " << result << std::endl;
+        }
+
+    }
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Total: " << result << std::endl;
+    return result;
+}
+
 int main()
 {
 
     find_first_vowel(std::vector<char>{});
+    calculate_pairs(std::vector<int>{1, 2, 3, 4});
 	return 0;
 }
